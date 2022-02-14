d0f1e2693f712bdcdf64fe483a938afe3ab207d4
Igor Klein <thelastklein@gmail.com>
2022-02-13 23:18:11 -0300

    Página inicial e novos componentes

--- dkgame/src/App.js
+++ dkgame/src/App.js
@@ -1,12 +1,14 @@
-import './App.css';
-import {Card} from './components/card/card.js'
+import {BodyContainer, Container, GameButton1, GameButton2} from './App.style'
 import { Link } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Link to="/Card">Drunk Game</Link>
-    </div>
+    <BodyContainer>
+      <Container>
+        <GameButton1><Link to="/Card">Drunk Game</Link></GameButton1>
+        <GameButton2>Eu Nunca</GameButton2>
+      </Container>
+    </BodyContainer>
   );
 }
 
---
+++ dkgame/src/App.style.js
@@ -0,0 +1,51 @@
+import styled from "styled-components"
+
+export const BodyContainer = styled.div`
+    background-color: #00b4d8;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+
+
+`
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 360px;
+    height: 400px;
+    margin-top: 30px;
+    background-color: beige;
+
+
+`
+export const GameButton1 = styled.button`
+    width: 190px;
+    height: 50px;
+    margin-top: 20px;
+    background-color: #0077b6;
+    border-radius: 20px;
+    border: none;
+    color: beige;
+        
+        a {
+            text-decoration: none;
+            color: beige;
+            font-size: 20px;
+        }
+
+`
+
+export const GameButton2 = styled.button`
+    width: 190px;
+    height: 50px;
+    margin-top: 20px;
+    background-color: #0077b6;
+    border-radius: 20px;
+    border: none;
+    color: beige;
+
+`
+
