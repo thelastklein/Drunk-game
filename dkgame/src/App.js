175cce9abe1882f9b683bd921f27562f17fe9f2b
Igor Klein <thelastklein@gmail.com>
2022-02-15 15:06:01 -0300

    estilizando pagina inicial

--- dkgame/src/App.js
+++ dkgame/src/App.js
@@ -1,12 +1,25 @@
 import {BodyContainer, Container, GameButton1, GameButton2} from './App.style'
 import { Link } from "react-router-dom";
+import cerveja from './assets/cerveja.png'
+import fogo from './assets/fogo.png'
 
 function App() {
   return (
     <BodyContainer>
       <Container>
-        <GameButton1><Link to="/Card">Drunk Game</Link></GameButton1>
-        <GameButton2><Link to="/Card2">Eu Nunca</Link></GameButton2>
+
+        <GameButton1> 
+          <img className='cerveja' src={cerveja} alt="cerveja"/>
+          <Link to="/Card">Jogo da Bebida</Link>
+          <img className='cerveja' src={cerveja} alt="cerveja"/>
+        </GameButton1>
+
+        <GameButton2>
+          <img className='fogo' src={fogo} alt="fogo"/>
+          <Link to="/Card2">Jogo eu Nunca</Link>
+          <img className='fogo' src={fogo} alt="fogo"/>
+        </GameButton2>
+
       </Container>
     </BodyContainer>
   );
--- dkgame/src/App.style.js
+++ dkgame/src/App.style.js
@@ -1,7 +1,7 @@
 import styled from "styled-components"
 
 export const BodyContainer = styled.div`
-    background-color: #00b4d8;
+    background-color: beige;
     height: 100vh;
     display: flex;
     align-items: center;
@@ -16,13 +16,15 @@ export const Container = styled.div`
     align-items: center;
     width: 360px;
     height: 400px;
-    margin-top: 30px;
+    margin-top: 200px;
     background-color: beige;
 
 
 `
 export const GameButton1 = styled.button`
-    width: 190px;
+    display:flex;
+    justify-content: center;
+    width: 300px;
     height: 50px;
     margin-top: 20px;
     background-color: #0077b6;
@@ -34,18 +36,39 @@ export const GameButton1 = styled.button`
             text-decoration: none;
             color: beige;
             font-size: 20px;
+            margin-top: 10px
+        }
+
+        .cerveja {
+            width: 50px;
+            
         }
 
 `
 
 export const GameButton2 = styled.button`
-    width: 190px;
+    display:flex;
+    justify-content: center;
+    width: 300px;
     height: 50px;
     margin-top: 20px;
-    background-color: #0077b6;
+    background-color: #a70000;
     border-radius: 20px;
     border: none;
     color: beige;
+
+    a {
+        text-decoration: none;
+        color: beige;
+        font-size: 20px;
+        margin-top: 10px;
+    }
+
+    .fogo {
+            width: 50px;
+            height: 40px;
+            margin-top: 3px;
+        }
 
 `
 
